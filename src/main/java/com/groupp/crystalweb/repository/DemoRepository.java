201339889347c65eee6c9a05fe065df31d8456b9
VFdo <vidufernando98@gmail.com>
2023-11-13 16:08:18 +0530

    Change from CrudRepository to JpaRepository

--- src/main/java/com/groupp/crystalweb/repository/DemoRepository.java
+++ src/main/java/com/groupp/crystalweb/repository/DemoRepository.java
@@ -1,13 +1,13 @@
 package com.groupp.crystalweb.repository;
 
 import com.groupp.crystalweb.entity.Demo;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface DemoRepository extends CrudRepository<Demo, String> {
+public interface DemoRepository extends JpaRepository<Demo, String> {
     Optional<Demo> findByRefId(String refId);
     long deleteByRefId(String refId);
 }

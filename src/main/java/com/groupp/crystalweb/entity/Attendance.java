25fc5845f5c1037df37a023df36a1025b91f85ba
ASUS <janithkulatunge@gmail.com>
2023-12-12 06:38:11 +0530

    Common API and DB implemented

--- src/main/java/com/groupp/crystalweb/entity/Attendance.java
+++ src/main/java/com/groupp/crystalweb/entity/Attendance.java
@@ -1,28 +1,40 @@
 package com.groupp.crystalweb.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Builder
 @Table(name = "attendance")
-public class Attendance {
-    @Id
-    private String refId;
-    private String employeeRefId;
-    private LocalDate date;
+public class Attendance extends SerializableObject {
+
+    //private String refId;
+    //@NotBlank(message = "Employee ID is required")
+    private Employee employee;
+
+    @NotNull(message = "Check in time is Required")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime inTime;
+
+    @NotNull(message = "Check out time is Required")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime outTime;
+
+
     private int overTimeHours;
+
     private String notes;
 }

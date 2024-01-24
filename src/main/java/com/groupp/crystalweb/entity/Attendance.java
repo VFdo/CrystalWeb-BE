24acccc0340e3d1d3546a4448d6c538b12f930da
VFdo <vidufernando98@gmail.com>
2024-01-24 16:28:32 +0530

    Attendance review fixes

--- src/main/java/com/groupp/crystalweb/entity/Attendance.java
+++ src/main/java/com/groupp/crystalweb/entity/Attendance.java
@@ -1,14 +1,14 @@
 package com.groupp.crystalweb.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.groupp.crystalweb.common.DateFormats;
 import jakarta.persistence.Entity;
 
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -21,18 +21,17 @@ import java.time.LocalDateTime;
 @Table(name = "attendance")
 public class Attendance extends SerializableObject {
 
-    //private String refId;
-    //@NotBlank(message = "Employee ID is required")
+    @ManyToOne
+    @JsonIgnore
     private Employee employee;
 
     @NotNull(message = "Check in time is Required")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonFormat(pattern = DateFormats.LOCAL_DATE_TIME)
     private LocalDateTime inTime;
 
     @NotNull(message = "Check out time is Required")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonFormat(pattern = DateFormats.LOCAL_DATE_TIME)
     private LocalDateTime outTime;
-
 
     private int overTimeHours;
 

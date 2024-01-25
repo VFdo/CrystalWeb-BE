5dbcd3c14a589308bf9d3f0bd3d774c6675725d2
VFdo <vidufernando98@gmail.com>
2024-01-25 08:05:08 +0530

    Appointment review changes

--- src/main/java/com/groupp/crystalweb/entity/Appointment.java
+++ src/main/java/com/groupp/crystalweb/entity/Appointment.java
@@ -1,6 +1,8 @@
 package com.groupp.crystalweb.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.groupp.crystalweb.common.DateFormats;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -8,7 +10,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -20,16 +21,23 @@ import java.time.LocalDateTime;
 public class Appointment extends SerializableObject {
 
     @NotNull(message = "Check in time is Required")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonFormat(pattern = DateFormats.LOCAL_DATE_TIME)
     private LocalDateTime date;
+
+    @NotNull(message = "Client ID must be present")
     @ManyToOne
+    @JsonIgnore
     private Client client;
+
     @ManyToOne
+    @JsonIgnore
     private Employee employee;
+
     @OneToOne
     private Bill bill;
 
     @ManyToOne
+    @JsonIgnore
     private Pet pet;
 
     private Status status;

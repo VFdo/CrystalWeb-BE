556b2cfdf0f4eefc209f96f9bd1f17e51d35f1ca
VFdo <vidufernando98@gmail.com>
2024-01-27 12:21:24 +0530

    Adding common api response to vet services

--- src/main/java/com/groupp/crystalweb/entity/VetService.java
+++ src/main/java/com/groupp/crystalweb/entity/VetService.java
@@ -1,23 +1,30 @@
 package com.groupp.crystalweb.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "vetService")
-public class VetService {
-    @Id
-    private String refId;
+@Table(name = "vet_service")
+public class VetService extends SerializableObject{
+    @NotBlank(message = "Service name is required")
     private String name;
-    private Float avgTime;
+
+    @NotNull(message = "isChargeByTime is required")
+    private Boolean chargeByTime;
+
+//    TODO: convert to enum - not urgent
+    @NotBlank(message = "Duration measure is required: hours/days")
+    private String durationMeasure; // hours/days
+
+    @NotNull(message = "Amount per duration is required")
     private Float amount;
 }

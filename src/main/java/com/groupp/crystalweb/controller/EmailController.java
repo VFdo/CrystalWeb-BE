96c6ffcab7f40c405f36554cb03822f185d24f04
ASUS <janithkulatunge@gmail.com>
2024-02-05 13:58:32 +0530

    Email changes made

--- src/main/java/com/groupp/crystalweb/controller/EmailController.java
+++ src/main/java/com/groupp/crystalweb/controller/EmailController.java
@@ -12,11 +12,13 @@ public class EmailController {
     private  EmailSenderService emailSenderService;
 
     public EmailController(EmailSenderService emailSenderService) {
+
         this.emailSenderService = emailSenderService;
     }
 
     @PostMapping("/send")
     public String sendEmail(@RequestParam(value = "file",required = false)MultipartFile[] file,String to,String[] cc, String subject, String body){
+        System.out.println("");
         return emailSenderService.sendEmail(file, to, cc, subject, body);
     }
 }
